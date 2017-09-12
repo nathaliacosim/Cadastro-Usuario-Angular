6a6be1d1bf65f34018341652baa12c62b001f25b
nathaliacosim <nathalia.cosim@novaandradina.org>
2017-09-12 10:25:25 -0400

    Cadastro-Usuario-Angular

--- js/controll.js
+++ js/controll.js
@@ -1,10 +1,95 @@
 angular.module("cadastro", []);
 angular.module("cadastro").controller("cadastroCtrl", function ($scope) {
     $scope.app = "Cadastro de Usuários";
-    $scope.cadUser = [ /*Contatos*/
-        {nome: "Nathalia", end: "R Bataiporã, 1130", endAlt: "Não existe", cidade: "NA", uf: "MS", email: "nath@gmail.com", emailAlt: "nathalia.cosim@gmail.com", telefone: "9 9999-9999", telAlt: "8 8888-8888"},
-        {nome: "João", end: "R Ivinhema, 5000", endAlt: "Não existe", cidade: "NA", uf: "MS", email: "joao@gmail.com", emailAlt: "joaozinho@gmail.com", telefone: "9 9999-9999", telAlt: "8 8888-8888"},
-        {nome: "Maria", end: "R Bataguassu, 8000", endAlt: "Não existe", cidade: "NA", uf: "MS", email: "maria@gmail.com", emailAlt: "mariatereza@gmail.com", telefone: "9 9999-9999", telAlt: "8 8888-8888"}
+    $scope.cadUser = [
+        {
+          nome: "João", 
+          end: "R Ivinhema, 1000", 
+          endAlt: "Não existe", 
+          cidade: "N.A.", 
+          uf: "MS", 
+          email: "joao@gmail.com", 
+          emailAlt: "joaozinho@gmail.com", 
+          telefone: "9 9999-9999", 
+          telAlt: "8 8888-8888"
+        },
+        {
+          nome: "Sayuri", 
+          end: "R São José, 2000", 
+          endAlt: "Não existe", 
+          cidade: "N.A.", 
+          uf: "MS", 
+          email: "gabs@gmail.com", 
+          emailAlt: "sayu@gmail.com", 
+          telefone: "9 9999-9999", 
+          telAlt: "8 8888-8888"
+        },
+        {
+          nome: "Pacifico", 
+          end: "R João de Lima, 30", 
+          endAlt: "Não existe", 
+          cidade: "N.A.", 
+          uf: "MS", 
+          email: "oceano@gmail.com", 
+          emailAlt: "revolts@gmail.com", 
+          telefone: "9 9999-9999", 
+          telAlt: "8 8888-8888"
+        },
+        {
+          nome: "Renatão", 
+          end: "R André Loyer, 4000", 
+          endAlt: "Não existe", 
+          cidade: "N.A.", 
+          uf: "MS", 
+          email: "renato@gmail.com", 
+          emailAlt: "renatao@gmail.com", 
+          telefone: "9 9999-9999", 
+          telAlt: "8 8888-8888"
+        },
+        {
+          nome: "Sonia", 
+          end: "R Johann Gill, 851", 
+          endAlt: "Não existe", 
+          cidade: "N.A.", 
+          uf: "MS", 
+          email: "soniawhite@gmail.com", 
+          emailAlt: "neve@gmail.com", 
+          telefone: "9 7070-8080", 
+          telAlt: "8 8080-7070"
+        },
+        {
+          nome: "Maria", 
+          end: "R Bataguassu, 8000", 
+          endAlt: "Não existe", 
+          cidade: "N.A.", 
+          uf: "MS", 
+          email: "maria@gmail.com", 
+          emailAlt: "mariatereza@gmail.com", 
+          telefone: "9 9999-9999", 
+          telAlt: "8 8888-8888"
+        },
+        {
+          nome: "Cayo", 
+          end: "R dos Bobos, 0", 
+          endAlt: "Não existe", 
+          cidade: "BATA", 
+          uf: "MS", 
+          email: "cayo@gmail.com", 
+          emailAlt: "cayozao@gmail.com", 
+          telefone: "9 9999-9999", 
+          telAlt: "8 8888-8888"
+        },
+        {
+          nome: "Joelma", 
+          end: "R Lua Traíra, 5000", 
+          endAlt: "Não existe", 
+          cidade: "BELÉM", 
+          uf: "PA", 
+          email: "joelma@gmail.com", 
+          emailAlt: "ximbinha@gmail.com", 
+          telefone: "9 7070-8080", 
+          telAlt: "8 8080-7070"
+        }
     ];
     $scope.adicionarUsuario = function (user) {
         $scope.cadUser.push(angular.copy(user));
@@ -20,4 +105,4 @@ angular.module("cadastro").controller("cadastroCtrl", function ($scope) {
             return user.selecionado;
         });
     };
-});
+});     
